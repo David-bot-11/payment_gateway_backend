a47a6daa1b69f05333da046190834f2dea0068b6
David-bot-11 <letscricket31@gmail.com>
2025-07-17 03:25:21 +0530

    initial stage

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit:"100kb"}));
 app.use(express.urlencoded({extended:true,limit:"100kb"}));
-app.use(cookieParser);
+app.use(cookieParser());
 

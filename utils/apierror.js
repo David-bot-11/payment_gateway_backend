b802d97d599549501232fc2e1f78775c98eb4a1e
David-bot-11 <letscricket31@gmail.com>
2025-08-25 00:52:23 +0530

    Add apierror, apiresponse, and asynchandler utility classes

---
+++ utils/apierror.js
@@ -0,0 +1,25 @@
+class apierror extends Error{
+constructor(
+    statusCode,
+    message="Something is wrong",
+    error=[],
+    stack=""
+){
+    super(message)
+    this.statusCode=statusCode,
+    this.data=null,
+    this.message=message,
+    this.success= false,
+    this.errors=errors
+    if(stack){
+        this.stack=stack
+    }
+    else{
+        Error.captureStackTrace(this,this.constructor)
+    }
+
+}
+
+}
+
+export{apierror}
